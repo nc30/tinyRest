204190dc7341660d2cb3e821c274df8ccd57d92d
Asahi Himura <himura@nitolab.com>
2023-03-22 12:16:47 +0900

    add middlware handler

--- tinyRest.go
+++ tinyRest.go
@@ -5,34 +5,41 @@ import (
 	"strings"
 )
 
+type Middlewares []func(http.Handler) http.Handler
+
+func (m Middlewares) Use(middlewares ...func(http.Handler) http.Handler) Middlewares {
+	m = append(m, middlewares...)
+	return m
+}
+
 func New(s *ResourceSet) *Resource {
 	r := &Resource{
-		methods: map[string]http.HandlerFunc{},
+		methods: map[string]http.Handler{},
 	}
 
 	if s.Get != nil {
-		r.methods[http.MethodGet] = s.Get
+		r.methods[http.MethodGet] = Chain(s.Get, s.Middlewares...)
 	}
 	if s.Head != nil {
-		r.methods[http.MethodHead] = s.Head
+		r.methods[http.MethodHead] = Chain(s.Head, s.Middlewares...)
 	}
 	if s.Post != nil {
-		r.methods[http.MethodPost] = s.Post
+		r.methods[http.MethodPost] = Chain(s.Post, s.Middlewares...)
 	}
 	if s.Put != nil {
-		r.methods[http.MethodPut] = s.Put
+		r.methods[http.MethodPut] = Chain(s.Put, s.Middlewares...)
 	}
 	if s.Patch != nil {
-		r.methods[http.MethodPatch] = s.Patch
+		r.methods[http.MethodPatch] = Chain(s.Patch, s.Middlewares...)
 	}
 	if s.Connect != nil {
-		r.methods[http.MethodConnect] = s.Connect
+		r.methods[http.MethodConnect] = Chain(s.Connect, s.Middlewares...)
 	}
 	if s.Delete != nil {
-		r.methods[http.MethodDelete] = s.Delete
+		r.methods[http.MethodDelete] = Chain(s.Delete, s.Middlewares...)
 	}
 	if s.Trace != nil {
-		r.methods[http.MethodTrace] = s.Trace
+		r.methods[http.MethodTrace] = Chain(s.Trace, s.Middlewares...)
 	}
 
 	r.AllowMethods()
@@ -41,7 +48,7 @@ func New(s *ResourceSet) *Resource {
 }
 
 type Resource struct {
-	methods map[string]http.HandlerFunc
+	methods map[string]http.Handler
 	cors    *string
 }
 
@@ -79,7 +86,7 @@ func (s *Resource) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	f(w, r)
+	f.ServeHTTP(w, r)
 }
 
 type ResourceSet struct {
@@ -91,4 +98,17 @@ type ResourceSet struct {
 	Connect http.HandlerFunc
 	Delete  http.HandlerFunc
 	Trace   http.HandlerFunc
+
+	Middlewares []func(http.Handler) http.Handler
+}
+
+func Chain(f func(http.ResponseWriter, *http.Request), middlewares ...func(http.Handler) http.Handler) http.Handler {
+	var fn http.Handler
+	fn = http.HandlerFunc(f)
+
+	for _, middleware := range middlewares {
+		fn = middleware(fn)
+	}
+
+	return fn
 }
